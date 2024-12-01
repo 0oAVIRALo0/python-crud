48e826770d4f9682e01e30d2ded4275070dbb24a
Aviral Chauhan <aviralchauhan@aviral.local>
2024-12-01 23:21:40 +0530

    Added MongoDB uri

--- config/database.py
+++ config/database.py
@@ -1,6 +1,13 @@
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 
-client = MongoClient("mongodb://localhost:27017/")
+load_dotenv()
+
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+print("MONGO PASSWORD: ", MONGO_PASSWORD)
+
+client = MongoClient(f"mongodb+srv://aviralch30:{MONGO_PASSWORD}@cosmocloud-backend.cnjq0.mongodb.net/?retryWrites=true&w=majority&appName=cosmocloud-backend")
 
 db = client.student_management_system
 
